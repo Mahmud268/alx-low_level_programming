25235694d656ea2c4126b8f600071f6be29b9876
mahmud <abubakarmahmud376@gmail.com>
2022-12-09 14:40:36 -0800

    m

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	char my;
+	
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	for (my = 'a'; my <= 'f'; i++)
+		putchar(my);
+	putchar('\n');
+
+	return (0);
+}
