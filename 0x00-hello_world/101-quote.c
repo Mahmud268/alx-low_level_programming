335fe49522ffaf55572dae103e8c3c20a592413c
mahmud <abubakarmahmud376@gmail.com>
2022-12-08 16:53:37 -0800

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
+ * main - Prints out the lastpart of a qoute in the standard error.
  *
- * Return: Always 1 (success)
+ * Return: 1 if successe.
  */
 
 int main(void)
 {
-	write(STDOUT_FILENO, " and that piece of art is useful \" - Dora Korpar, 2015-10-19\n");
+	write(2, " and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
