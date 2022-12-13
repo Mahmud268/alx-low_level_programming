a8f4d37e694ec317f9644b0db461b60b92c82b1b
mahmud <abubakarmahmud376@gmail.com>
2022-12-13 14:11:15 -0800

    m

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print _putchar
+ * is written by me
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+return (0);
+}
