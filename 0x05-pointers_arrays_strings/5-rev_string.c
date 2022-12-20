56bbbd7cfdd17c48bd6a81682e77b14db1281ffa
mahmud <abubakarmahmud376@gmail.com>
2022-12-19 21:07:03 -0800

    m

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,7 +2,7 @@
 
 /**
  * rev_string - check main
- *
+ * @s: The string to be checked
  * Return: Always 0
  */
 void rev_string(char *s)
@@ -15,11 +15,11 @@ void rev_string(char *s)
 	while (s[i] != '\0')
 	{
 		j++;
-{
+	}
 	j--;
 	while (j > i)
 
-}
+	{
 		rev = s[j];
 		s[j--] = s[i];
 		s[i++] = rev;
